516a49988a6dcbbc5af9e43cd8b1b0f0b2c8df4a
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-04-21 15:49:52 -0500

    restructed schema for ease of use in calculations

--- Develop/models/Workout.js
+++ Develop/models/Workout.js
@@ -1,37 +1,36 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
+const Exercise = new Schema({
+	type: {
+		type: String,
+	},
+	name: {
+		type: String,
+	},
+	duration: {
+		type: Number,
+	},
+	distance: {
+		type: Number
+	},
+	weight: {
+		type: Number
+	},
+	reps: {
+		type: Number
+	},
+	sets: {
+		type: Number
+	}
+});
 const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    },
-    exercise: [{
-        name: {
-            type: String,
-            trim: true,
-            required: "You must provide a name for the exercise"
-        },
-        type: {
-            type: String,
-            trim: true,
-            required: "You must provide an exercise type"
-        },
-        weight: {
-            type: Number
-        },
-        sets: {
-            type: Number
-        },
-        reps: {
-            type: Number
-        },
-        duration: {
-            type: Number
-        }
-    }]
+	day: {
+			type:Date,
+			default:Date.now
+	},
+	exercises: [Exercise]
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
-module.exports = Workout;
+const WorkoutModel = mongoose.model("Workout", WorkoutSchema);
+module.exports = WorkoutModel;
