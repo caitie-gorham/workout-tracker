93ba8a765b78a836e37e7856b0930c2bb333c59a
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-04-21 15:52:34 -0500

    renamed paths in server to match routes

--- Develop/server.js
+++ Develop/server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const databaseUrl = "workoutdb";
-const collections = ["workout", "exercise"];
+const databaseUrl = "workout";
+const collections = ["workout", "workouts"];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,11 +16,16 @@ app.use(express.static("public"));
 app.use(require("./routes/htmlRoutes"));
 app.use(require("./routes/apiRoutes"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
