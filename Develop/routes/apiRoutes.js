75b5838e039073573eaaa5a84af711c98dce5668
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-04-21 15:51:28 -0500

    redid api routes, still not working correctly

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,66 +1,67 @@
 const router = require("express").Router();
 const db = require("../models");
 
-router.get('api/workouts', (req, res) => {
-    db.Workout.find({}).then(dbWorkout => {
-        res.json(dbWorkout);
+router.get("/api/workouts", (req, res) => {
+  db.WorkoutModel.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
-        .catch(err => {
-            res.json(err);
-        });
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 router.put("/api/workouts/:id", ({ params: { id }, body }, res) => {
-    console.log('body', body);
-    db.Workout.findOneAndUpdate({ _id: id }, { $push: { exercises: body } }, { new: true })
-        .then(dbWorkout => {
-            console.log('dbWorkout', dbWorkout);
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            console.log('err', err);
-            res.json(err);
-        });
+  console.log('body', body);
+  db.WorkoutModel.findOneAndUpdate({ _id: id }, { $push: { exercise: body } }, { new: true })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      console.log('err', err);
+      res.json(err);
+    });
 });
 
 router.post("/api/workouts", ({ body }, res) => {
-    db.Workout.create(body)
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+  db.WorkoutModel.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 router.get("/api/workouts/range", (req, res) => {
-    db.Workout.aggregate([
-        {
-            $addFields: {
-                totalDuration: {
-                    $sum: "$duration"
-                },
-                totalDistance: {
-                    $sum: "$distance"
-                },
-                totalWeight: {
-                    $sum: "$weight"
-                },
-                totalReps: {
-                    $sum: "$reps"
-                },
-                totalSets: {
-                    $sum: "$sets"
-                }
-            }
-        }])
-        .limit(7)
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+  db.WorkoutModel.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercise.duration"
+        },
+        totalDistance: {
+          $sum: "$exercise.distance"
+        },
+        totalWeight: {
+          $sum: "$exercise.weight"
+        },
+        totalReps: {
+          $sum: "$exercise.reps"
+        },
+        totalSets: {
+          $sum: "$exercise.sets"
+        }
+      }
+    }])
+    .limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
+
 
 module.exports = router;
